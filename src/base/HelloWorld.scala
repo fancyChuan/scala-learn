ab25c33b81fda1ed38f639e1dd6e4e36ef8465e6
fancyChuan <1247375074@qq.com>
2019-04-23 21:07:28 +0800

    Scala：循环、数组访问

--- src/base/HelloWorld.scala
+++ src/base/HelloWorld.scala
@@ -2,7 +2,37 @@ package base
 
 object HelloWorld {
 
-  def main(args: Array[String]) = {
-    print("hello world")
+  // 有返回值的的写法
+  def max(a:Int, b:Int): Int = { // 返回Int类型
+    if (a > b) a
+    else
+      b
+  }
+
+  // 无返回值
+  def greet(): Unit = println("hello scala")
+
+  // while实现循环
+  def loopPrint() = {
+    val args = Array("fancy", "chuan", "cool")
+    // 传统方式
+    var i = 0
+    while (i < args.length) {
+      println(args(i)) // 这里数组的写法是括号，而不是中括号！
+      i += 1
+    }
+
+    // 更优雅的方式
+    args.foreach(arg => print(arg + "\t"))
+  }
+
+
+
+
+  // 函数执行入口
+  def main(args: Array[String]) = { // 返回空类型
+    greet()
+    println(max(44, 33))
+    loopPrint()
   }
 }
