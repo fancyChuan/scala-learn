13f1cbba2bda2774c4924e8451ce3040c58429a8
fancyChuan <1247375074@qq.com>
2019-04-23 23:59:09 +0800

    数组的使用

--- src/base/HelloWorld.scala
+++ src/base/HelloWorld.scala
@@ -14,18 +14,27 @@ object HelloWorld {
 
   // while实现循环
   def loopPrint() = {
-    val args = Array("fancy", "chuan", "cool")
+    val args = Array("fancy", "chuan", "cool") // 等同于 Array.apply("")
     // 传统方式
     var i = 0
     while (i < args.length) {
       println(args(i)) // 这里数组的写法是括号，而不是中括号！
       i += 1
     }
-
     // 更优雅的方式
     args.foreach(arg => print(arg + "\t"))
+    // 更高级的方法
+    for (arg <- args) println(arg + "\t")
   }
 
+  // 数组也可以用泛型
+  def arrayTest(): Unit = {
+    val greets = new Array[String](3) // 泛型使用中括号而不是尖括号
+    greets(0) = "hello"
+    greets(1) = ","
+    greets.update(2, "world") // 也可以这样更新
+    for (i <- 0 to 2) print(greets(i))
+  }
 
 
 
@@ -34,5 +43,6 @@ object HelloWorld {
     greet()
     println(max(44, 33))
     loopPrint()
+    arrayTest()
   }
 }
